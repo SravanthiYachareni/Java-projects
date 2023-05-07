b7a6e31eb178229c0565454a6cbf8ef90d0afb6e
Sravanthi Yachareni <sravanthiyachareni@gmail.com>
2023-05-07 10:51:29 +0530

    added

---
+++ BasicProgram.java
@@ -0,0 +1,14 @@
+
+ 
+ class BasicProgram  {
+ int b= 8;
+ public static void main(String args[]) {
+ 
+ 
+ BasicProgram obj1 = new BasicProgram() ;
+ 
+
+  System.out.println(obj1.b);
+  
+  }
+  }
